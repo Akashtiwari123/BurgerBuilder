0782679da45da2d0246e16b57194401d801519ff
Akash Tiwari <drakashti@gmail.com>
2019-11-27 18:57:15 +0530

    burgerControls

--- src/component/Burger/Burger.js
+++ src/component/Burger/Burger.js
@@ -3,11 +3,26 @@ import "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  let transformedIngrediants = Object.keys(props.ingrediants)
+    .map(igKey => {
+      //igkey is having the key like salad, cheese
+      return [...Array(props.ingrediants[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngrediants.length === 0) {
+    transformedIngrediants = <p>Please add ingrediants</p>;
+  }
+
+  console.log(transformedIngrediants);
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="salad" />
+      {transformedIngrediants}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,19 @@ import Aux from "../../hoc/Aux";
 import Burger from "../../component/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingrediants: {
+      salad: 0,
+      cheese: 0,
+      meat: 0,
+      myonnaise: 0
+    }
+  };
   render() {
     return (
       <Aux>
         <div>
-          <Burger />
+          <Burger ingrediants={this.state.ingrediants} />
         </div>
         <div>Build Controls</div>
       </Aux>
