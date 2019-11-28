6f84635357c4cffb94eadc061a6f516270bd58f4
Akash Tiwari <drakashti@gmail.com>
2019-11-28 11:08:49 +0530

    add and remove Ing

--- src/component/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/component/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./BuildControl.css";
+import BuildControls from "../BuildControls";
+
+const removeIng = () => {};
+
+const addIng = () => {};
 
 const buildControl = props => (
   <div className="BuildControl">
     <div className="Label"> {props.label}</div>
-    <button className="Less">Less</button>
-    <button className="More">More</button>
+    <button className="Less" onClick={() => removeIng(props.type)}>
+      Less
+    </button>
+    <button className="More" onClick={() => addIng(props.type)}>
+      More
+    </button>
   </div>
 );
 
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,12 @@ import Aux from "../../hoc/Aux";
 import Burger from "../../component/Burger/Burger";
 import BuildControls from "../../component/Burger/BuildControls/BuildControls";
 
+const INGREDIANTS_PRICE = {
+  salad: 15,
+  cheese: 20,
+  meat: 25,
+  myonnaise: 15
+};
 class BurgerBuilder extends Component {
   state = {
     ingrediants: {
@@ -10,8 +16,32 @@ class BurgerBuilder extends Component {
       cheese: 1,
       meat: 1,
       myonnaise: 1
-    }
+    },
+    totalPrice: 10
   };
+
+  addIng = type => {
+    const oldCount = this.state.ingrediants[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngrediant = [...this.state.ingrediants];
+    updatedIngrediant[type] = updatedCount;
+    const priceAddition = INGREDIANTS_PRICE[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = priceAddition + oldPrice;
+    this.setState({ totalPrice: newPrice, ingrediants: updatedIngrediant });
+  };
+
+  removeIng = type => {
+    const oldCount = this.state.ingrediants;
+    const updatedCount = oldCount - 1;
+    const updatedIngrediant = [...this.state.ingrediants];
+    updatedIngrediant[type] = updatedCount;
+    const priceDeduction = INGREDIANTS_PRICE[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingrediants: updatedIngrediant });
+  };
+
   render() {
     return (
       <Aux>
