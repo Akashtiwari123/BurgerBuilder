c4a15edb7c469725673680091659adaa4e12a7cc
Akash Tiwari <drakashti@gmail.com>
2019-11-28 08:02:26 +0530

    Minor

--- src/component/Burger/BuildControls/BuildControls.js
+++ src/component/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Myonnaise", type: "myonnaise" },
+  { label: "Meat", type: "meat" }
+];
 
 const buildControls = props => {
-  return <div />;
+  return (
+    <div className="BuildControls">
+      {controls.map(ctrl => (
+        <BuildControl key={ctrl.label} label={ctrl.label} />
+      ))}
+    </div>
+  );
 };
 
 export default buildControls;
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../component/Burger/Burger";
+import BuildControls from "../../component/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
   state = {
     ingrediants: {
-      salad: 0,
-      cheese: 0,
-      meat: 0,
-      myonnaise: 0
+      salad: 2,
+      cheese: 1,
+      meat: 1,
+      myonnaise: 1
     }
   };
   render() {
@@ -17,7 +18,7 @@ class BurgerBuilder extends Component {
         <div>
           <Burger ingrediants={this.state.ingrediants} />
         </div>
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux>
     );
   }
