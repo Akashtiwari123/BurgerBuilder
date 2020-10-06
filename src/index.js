f604b8534c667796715164938b110ac4766c01eb
Akash Tiwari <drakashti@gmail.com>
2020-10-06 12:27:22 +0530

    Major Change

--- src/index.js
+++ src/index.js
@@ -3,19 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import reducer from "./store/reducer";
-import { createStore } from "redux";
-
-const store = createStore(reducer);
 
 const rootElement = document.getElementById("root");
 const app = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-      {console.log("Redux There You Are!!")}
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 ReactDOM.render(app, rootElement);
